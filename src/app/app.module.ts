7cc73c37c5ac184e9f06939c8f749ea93a4a2923
Stephen Hanjaya <stephenhanjaya@gmail.com>
2023-09-05 22:34:31 -0700

    add list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WrapperComponent } from './wrapper/wrapper.component';
-
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, WrapperComponent],
+  declarations: [AppComponent, WrapperComponent, ListComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
